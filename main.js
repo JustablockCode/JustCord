efe5971683480ad31bb61ed47a8aef9fd91a9057
Justablock <bdfd000@gmail.com>
2025-01-27 15:21:02 +0100

    some shtuff

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("test")
+const { app, BrowserWindow } = require('electron/main')
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'assets', 'icons', 'icon.png')
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
